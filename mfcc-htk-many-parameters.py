f0250591d9f41e09e57c3c8b10106d46f69f3234
georgid <georgi.dzhambazov@upf.edu>
2017-12-06 19:26:13 +0100

    modified preemphasis coefficients

--- mfcc-htk-many-parameters.py
+++ mfcc-htk-many-parameters.py
@@ -1,74 +1,107 @@
 
-# coding: utf-8
+# this script (almost) reproduces htk-mfccs with configuration  config_wav_to_mfcc_2 as used in the paper 
+# A. Kruspe - BOOTSTRAPPING A SYSTEM FOR PHONEME RECOGNITION AND KEYWORD SPOTTING IN UNACCOMPANIED SINGING
 
-# In[15]:
 
 import extract_mfcc_htk_way as essMFCC_
 import HTKPython as HTK
 import numpy as np
 import matplotlib.pyplot as plt
 import librosa
-
+import sys
 # In[11]:
 
-########### extract filterbank with htk
+########### extract mfcc with htk 
 
-URImfcFile_no_preemp = 'HTK/vignesh.config_wav_to_mfcc_2' # with cpestral  mean norm
-HTKFeat_reader_no_preemp =  HTK.htk_open(URImfcFile_no_preemp, 'rb')
-htkMFCC_no_preemp = HTKFeat_reader_no_preemp.getall()
-htkMFCC_no_preemp = htkMFCC_no_preemp.T
+def doit(argv):
+#     if len(argv) != 2:
+#         sys.exit('usage: {} <audio_URI>'.format(argv[0]))
+#     audio_URI  = argv[1]
+    
+    URImfcFile = 'HTK/vignesh.config_wav_to_mfcc_2' # for configuration MFCC_0_A_D_Z
+    audio_URI = 'audio/vignesh.wav'
+    
+    URImfcFile = 'HTK/trick_me_vocal.config_wav_to_mfcc_2' # for configuration MFCC_0_A_D_Z
+    audio_URI = '/Users/joro/Dropbox/HansensDataset/vocal_separated/trick_me_vocals.wav'
+    
+    URImfcFile = 'HTK/trick_me_acap.config_wav_to_mfcc_2' # for configuration MFCC_0_A_D_Z
+    audio_URI = '/Users/joro/Dropbox/HansensDataset/aCappella/trick_me.wav'
+    
+    
+    ### read mfccs extracted with htk
+    HTKFeat_reader =  HTK.htk_open(URImfcFile, 'rb')
+    htkMFCC = HTKFeat_reader.getall()
+    htkMFCC = htkMFCC.T
+    
 
-# dimension [d,t]
-essMFCC = essMFCC_.extractor('audio/vignesh.wav', 0) # putting zero is same as PREEMCOEF =  0 in htk
-essMFCC = essMFCC_.extractor('audio/vignesh.wav', 0.97) # the scale of the difference in essentia is very small compared to htk
-essMFCC = essMFCC[[1,2,3,4,5,6,7,8,9,10,11,12,0],:] # append energy at the end as in htk 
-
-delta_mfcc = librosa.feature.delta(essMFCC)
-delta_delta_mfcc = librosa.feature.delta(essMFCC, order=2)
-
-essMFCC = np.vstack((essMFCC, delta_mfcc))
-essMFCC = np.vstack((essMFCC, delta_delta_mfcc))
-
-# cepstral zero-mean
-essMFCC  = np.matrix(essMFCC)
-essMFCC = essMFCC - essMFCC.mean(axis=1)
-
-
-
-
-# Plotting comparison of MFCCs and their difference 
-
-fig, axis_array= plt.subplots(3, sharex=True)
-axis_array[0].imshow(essMFCC, aspect = 'auto', interpolation='none') # ignore enery
-axis_array[0].set_title('essentia')
-
-p = axis_array[1].imshow(htkMFCC_no_preemp, aspect = 'auto', interpolation='none') # ignore enery
-# axis_array[1].imshow(htkMFCC_no_preemp[1:,:], aspect = 'auto', interpolation='none') # ignore enery
-
-# plt.colorbar(p)
-axis_array[1].set_title('htk')
-
-p = axis_array[2].imshow(np.abs(htkMFCC_no_preemp - essMFCC), aspect = 'auto', interpolation='none') # ignore enery
-plt.colorbar(p)
-plt.show()
-
-
-# In[18]:
-
-# Ploting HTK only
-# plt.imshow(htkMFCC[1:,:], aspect = 'auto', interpolation='none') # ignore enery
-# plt.title('MFCC HTK')
-# plt.colorbar()
-# plt.show() # unnecessary if you started "ipython --pylab"
+    # dimension [d,t]
+    print 'extracting mfccs for file {}...'.format(audio_URI)
+    essMFCC = essMFCC_.extractor(audio_URI, 0.97) # the scale of the difference in essentia is very small compared to htk
+    essMFCC = essMFCC[[1,2,3,4,5,6,7,8,9,10,11,12,0],:] # append energy at the end as it is in htk 
+    
+    delta_mfcc = librosa.feature.delta(essMFCC)
+    delta_delta_mfcc = librosa.feature.delta(essMFCC, order=2)
+    
+    essMFCC = np.vstack((essMFCC, delta_mfcc))
+    essMFCC = np.vstack((essMFCC, delta_delta_mfcc))
+    
+    # cepstral zero-mean
+    essMFCC  = np.matrix(essMFCC)
+    means = essMFCC.mean(axis=1)
+    # if np.isnan(means).any(): # this hsould not happen
+    #     print 'mean has nans'.format()
+    
+    essMFCC = essMFCC - means
+    
+    
+    # plot without  deltas and delta deltas
+    htkMFCC = htkMFCC[:12,:]
+    essMFCC = essMFCC[:12,:]
+    plot_mfccs_and_differernce(htkMFCC, essMFCC)
 
 
-# Plotting the difference
+def plot_mfccs_and_differernce(htkMFCC, essMFCC):
 
-# In[4]:
+    ''' 
+    Plotting comparison of MFCCs and their difference
+    ''' 
+    
+    fig, axis_array= plt.subplots(3, sharex=True)
+    axis_array[0].imshow(essMFCC, aspect = 'auto', interpolation='none') # ignore enery
+    # plt.colorbar(p)
+    axis_array[0].set_title('essentia')
+    
+    axis_array[1].imshow(htkMFCC, aspect = 'auto', interpolation='none') # ignore enery
+    # axis_array[1].imshow(htkMFCC[1:,:], aspect = 'auto', interpolation='none') # ignore enery
+    # plt.colorbar(p)
+    axis_array[1].set_title('htk')
+    
+    spectral_difference = np.abs(htkMFCC - essMFCC)
+    p = axis_array[2].imshow(spectral_difference, aspect = 'auto', interpolation='none') # ignore enery
+    plt.colorbar(p)
+    plt.show()
+    
+    print np.sum(spectral_difference)
 
-# Ploting Difference only
-# plt.imshow(np.abs(essMFCC- htkMFCC[1:,:]), aspect = 'auto', interpolation='none') # ignore enery
-# plt.title('MFCC Difference')
-# plt.colorbar()
-# plt.show() # unnecessary if you started "ipython --pylab"
+    # In[18]:
+    
+    # Ploting HTK only
+    # plt.imshow(htkMFCC[1:,:], aspect = 'auto', interpolation='none') # ignore enery
+    # plt.title('MFCC HTK')
+    # plt.colorbar()
+    # plt.show() # unnecessary if you started "ipython --pylab"
+    
+    
+    # Plotting the difference
+    
+    # In[4]:
+    
+    # Ploting Difference only
+    # plt.imshow(np.abs(essMFCC- htkMFCC[1:,:]), aspect = 'auto', interpolation='none') # ignore enery
+    # plt.title('MFCC Difference')
+    # plt.colorbar()
+    # plt.show() # unnecessary if you started "ipython --pylab"
 
+
+if __name__ == '__main__':
+    doit(sys.argv)
